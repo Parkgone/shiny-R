996e18120a002495062740ab6ca024c7b0c854ed
Parkjiwon <33209507+Parkgone@users.noreply.github.com>
2017-11-24 10:45:46 +0900

    Add files via upload
    실습

--- app.R
+++ app.R
@@ -7,36 +7,36 @@ library(mapproj)
 counties <- readRDS("counties.rds")
 
 # Source helper functions -----
-source("helpers.R")
+source("helpers.R") #함수들을 직접 사용하겠다.
 
 # User interface ----
 ui <- fluidPage(
   titlePanel("censusVis"),
   
-  sidebarLayout(
-    sidebarPanel(
+  sidebarLayout( #전체 레이아웃에
+    sidebarPanel( #패널을 만들고
       helpText("Create demographic maps with 
-               information from the 2010 US Census."),
+               information from the 2010 US Census."), #위에 설명문구 집어넣는것  
       
-      selectInput("var", 
+      selectInput("var", #사용자가 선택가능한것 selectinput은 인종 선택하는것 c를 통해서  단어를 명시
                   label = "Choose a variable to display",
                   choices = c("Percent White", "Percent Black",
                               "Percent Hispanic", "Percent Asian"),
-                  selected = "Percent White"),
+                  selected = "Percent White"), #초깃값 지정 키자마자 이거나오는것
       
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
+      sliderInput("range",  
+                  label = "Range of interest:", #변수 이름
+                  min = 0, max = 100, value = c(0, 100))  #사이드바는 사용자 움직임으로 인해 들어오는 변수 value = c(0, 100)는 처음 들어갔을떄의 초기값
       ),
     
-    mainPanel(plotOutput("map"))
+    mainPanel(plotOutput("map")) #서버에서 map으로 가시화
   )
   )
 
 # Server logic ----
 server <- function(input, output) {
   output$map <- renderPlot({
-    args <- switch(input$var,
+    args <- switch(input$var, #내 선택에 따라 다른결과가 나오도록
                    "Percent White" = list(counties$white, "darkgreen", "% White"),
                    "Percent Black" = list(counties$black, "black", "% Black"),
                    "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
@@ -45,8 +45,9 @@ server <- function(input, output) {
     args$min <- input$range[1]
     args$max <- input$range[2]
     
-    do.call(percent_map, args)
-  })
+    do.call(percent_map, args) #함수를 불러온다 r에서 씨나 자바나 부를때 쓴다. 그렇지만 알에 내장되어있는 함수를 호출할때도 쓴다.
+
+      })
 }
 # Run app ----
 shinyApp(ui, server)
--- helpers.R
+++ helpers.R
@@ -1,10 +1,10 @@
 # Note: percent map is designed to work with the counties data set
 # It may not work correctly with other data sets if their row order does 
 # not exactly match the order in which the maps package plots counties
-percent_map <- function(var, color, legend.title, min = 0, max = 100) {
+percent_map <- function(var, color, legend.title, min = 0, max = 100) { #var=인종 color=인종별로 다르게 표현
 
   # generate vector of fill colors for map
-  shades <- colorRampPalette(c("white", color))(100)
+  shades <- colorRampPalette(c("white", color))(100) #백인에 대해서 초록색으로 표현 초록색을 100단계로 나눠서 진한 밝기표현으로 백분율 표현
   
   # constrain gradient to percents that occur between min and max
   var <- pmax(var, min)
@@ -23,7 +23,7 @@ percent_map <- function(var, color, legend.title, min = 0, max = 100) {
     lty = 1, lwd = 1, projection = "polyconic", 
     myborder = 0, mar = c(0,0,0,0))
   
-  # add a legend
+  # add a legend  #5단계로 나눠지는 부분
   inc <- (max - min) / 4
   legend.text <- c(paste0(min, " % or less"),
     paste0(min + inc, " %"),
